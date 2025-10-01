fda68a560709d9fce53aabbecde69ce0f20b3576
doc <simone.milano12@gamil.com>
2025-10-01 15:35:30 +0100

    First commit

---
+++ SecureChat/SecureChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureChatApp.swift
+//  SecureChat
+//
+//  Created by doc on 01/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SecureChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

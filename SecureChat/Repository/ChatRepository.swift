f7e97bced38659488a7764cb1ea10c5f0869b893
doc <simone.milano12@gamil.com>
2025-10-03 11:03:36 +0100

    Injected chat repo into chat list vm

--- SecureChat/Repository/ChatRepository.swift
+++ SecureChat/Repository/ChatRepository.swift
@@ -9,6 +9,7 @@ import Foundation
 
 protocol ChatRepositoryProtocol {
     var chat: [Chat] { get }
+    var chatList: [ChatListModel] { get }
 }
 
 final class ChatRepository: ChatRepositoryProtocol {
@@ -42,4 +43,10 @@ final class ChatRepository: ChatRepositoryProtocol {
         
         return [chat1, chat2, chat3]
     }
+    
+    var chatList: [ChatListModel] = [
+        ChatListModel(chatID: UUID(uuidString: "578E8708-36DC-4820-86DF-4CB00A1EC8C8")!, title: "Chat 1", timestamp: Date.now.description),
+        ChatListModel(chatID: UUID(uuidString: "CA654CF5-862E-4FDA-8856-35B67564A07B")!, title: "Chat 2", timestamp: Date.now.description),
+        ChatListModel(chatID: UUID(uuidString: "73B44F3D-240E-4025-81E3-16B29B6333A7")!, title: "Chat 3", timestamp: Date.now.description)
+    ]
 }
--- SecureChat/ViewModel/ChatListViewModel.swift
+++ SecureChat/ViewModel/ChatListViewModel.swift
@@ -7,22 +7,25 @@
 
 import SwiftUI
 
-@Observable
+protocol ChatListViewModelProtocol {
+    var chatList: [ChatListModel] { get }
+}
 
-final class ChatListViewModel {
-    var chatList: [ChatListModel] = [
-        ChatListModel(chatID: UUID(uuidString: "578E8708-36DC-4820-86DF-4CB00A1EC8C8")!, title: "Chat 1", timestamp: Date.now.description),
-        ChatListModel(chatID: UUID(uuidString: "CA654CF5-862E-4FDA-8856-35B67564A07B")!, title: "Chat 2", timestamp: Date.now.description),
-        ChatListModel(chatID: UUID(uuidString: "73B44F3D-240E-4025-81E3-16B29B6333A7")!, title: "Chat 3", timestamp: Date.now.description)
-    ]
+@Observable
+final class ChatListViewModel: ChatListViewModelProtocol {
+
+    let chatRepo: ChatRepositoryProtocol
+    
+    init(chatRepo: ChatRepositoryProtocol = ChatRepository()) {
+        self.chatRepo = chatRepo
+    }
+    
+    var chatList: [ChatListModel] {
+        return chatRepo.chatList
+    }
     
     func printID(model: ChatListModel) {
         print(model.chatID)
     }
     
 }
-/*
-578E8708-36DC-4820-86DF-4CB00A1EC8C8
-CA654CF5-862E-4FDA-8856-35B67564A07B
-73B44F3D-240E-4025-81E3-16B29B6333A7
-*/
